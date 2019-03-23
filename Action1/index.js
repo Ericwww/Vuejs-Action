1189bea8003b0acbd65308be6035c183b331eee3
Ericwww <syasusu@163.com>
2019-03-23 20:03:40 +0800

    finish practice

--- Action1/index.js
+++ Action1/index.js
@@ -1,47 +1,80 @@
-var app=new Vue({
-    el:'#app',
-    data:{
-        list:[
-            {
-                id:1,
-                name:'iPhone 7',
-                price: 6188,
-                count: 1
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [{
+                prop: 'Electronic',
+                product: [{
+                        id: 1,
+                        name: 'iPhone 7',
+                        price: 6188,
+                        count: 1,
+                        checked: false
+                    },
+                    {
+                        id: 2,
+                        name: 'iPad Pro',
+                        price: 5888,
+                        count: 1,
+                        checked: false
+                    },
+                    {
+                        id: 3,
+                        name: 'Macbook Pro',
+                        price: 21488,
+                        count: 1,
+                        checked: false
+                    }
+                ]
             },
             {
-                id:2,
-                name:'iPad Pro',
-                price: 5888,
-                count: 1
+                prop: 'Life',
+                product: [{
+                    id: 1,
+                    name: 'Shower',
+                    price: 30,
+                    count: 1,
+                    checked: false
+                }]
             },
             {
-                id:3,
-                name:'Macbook Pro',
-                price: 21488,
-                count: 1
+                prop: 'Vegetable',
+                product: [{
+                    id: 1,
+                    name: 'Apple',
+                    price: 3,
+                    count: 1,
+                    checked: false
+                },]
             }
         ]
     },
     computed: {
-        totalPrice:function () {
-            var total=0;
-            for(var i=0;i<this.list.length;i++){
-                var item=this.list[i];
-                total += item.price * item.count;
+        totalPrice: function () {
+            var total = 0;
+            for (var i = 0; i < this.list.length; i++) {
+                var item = this.list[i];
+                if (item.checked) {
+                    total += item.price * item.count;
+                }
             }
-            return total.toString().replace(/\B(?=(\d{3})+$)/g,',');
+            return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
         }
     },
     methods: {
-        handleReduce:function (index) {
-            if(this.list[index].count===1) return;
+        handleReduce: function (index) {
+            if (this.list[index].count === 1) return;
             this.list[index].count--;
         },
-        handleAdd:function (index) {
+        handleAdd: function (index) {
             this.list[index].count++;
         },
-        handleRemove:function (index) {
-            this.list.splice(index,1);
+        handleRemove: function (index) {
+            this.list.splice(index, 1);
+        },
+        selectAll: function () {
+            for (let i = 0; i < this.list.length; i++) {
+                this.list[i].checked = true;
+            }
         }
     },
 })
