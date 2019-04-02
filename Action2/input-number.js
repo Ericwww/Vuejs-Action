bee0f711a4f38578591e79b39c8ecc666e68e5e2
Ericwww <syasusu@163.com>
2019-04-02 21:44:58 +0800

    finish action2‘s practice
    finish action2‘s practice

--- Action2/input-number.js
+++ Action2/input-number.js
@@ -5,7 +5,7 @@ function isValueNumber(value) {
 Vue.component('input-number', {
     template: '\
         <div class="input-number"> \
-            <input type="text" :value="currentValue" @change="handleChange"> \
+            <input type="text" :value="currentValue" @change="handleChange" ref="input" @keydown="keyDown($event)"> \
             <button @click="handleDown" :disabled="currentValue <= min">-</button> \
             <button @click="handleUp" :disabled="currentValue >= max">+</button> \
         </div>',
@@ -21,6 +21,10 @@ Vue.component('input-number', {
         value: {
             type: Number,
             default: 0
+        },
+        step:{
+            type:Number,
+            default:10
         }
     },
     data: function () {
@@ -45,11 +49,11 @@ Vue.component('input-number', {
         },
         handleDown: function () {
             if (this.currentValue <= this.min) return;
-            this.currentValue -= 1;
+            this.currentValue -= this.step;
         },
         handleUp: function () {
             if (this.currentValue >= this.max) return;
-            this.currentValue += 1;
+            this.currentValue += this.step;
         },
         handleChange: function (event) {
             var val = event.target.value.trim();
@@ -68,10 +72,23 @@ Vue.component('input-number', {
             } else {
                 event.target.value = this.currentValue;
             }
+        },
+        keyDown:function(ev){
+            if(ev.keyCode==38){
+                this.handleUp();
+            }else if (ev.keyCode==40) {
+                this.handleDown();
+            }
         }
 
     },
     mounted: function () {
         this.updateValue(this.value);
+        this.$refs.input.focus();
     }
 });
+/*
+若想实现功能，为input增加@keydown="keyDown($event)"，并增加keyDown来判断按下的按键即可实现
+这里我们还增加了 ref="input" 和在mounted中增加this.$refs.input.focus()来监听输入框的聚焦
+这样可以在网页挂载后自动聚焦输入框
+*/
